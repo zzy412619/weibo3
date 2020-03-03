9c1fec46515eaadbf68881437104e0b26c983856
zeyuan <zzy412619@qq.com>
2020-03-03 11:21:22 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,19 +19,12 @@ class UsersController extends Controller
     //创建注册用户
     public function store(Request $request)
     {
-        $this->validate($request, [
+        $this->validate($request,[
             'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
-            'password' => 'required|confirmed|min:6'
+            'password' => 'required|confirmed|min6'
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
-
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show', [$user]);
+        
     }
 }
