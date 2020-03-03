d5b1e5833f31bc095f4236303cdeaece8c5fc52c
zeyuan <zzy412619@qq.com>
2020-03-03 06:58:43 +0000

    初始化样式

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    //用户注册表单
+    public function signup()
+    {
+    	return view('users.signup');
+    }
+    public function show()
+    {
+    	return view('users.show',compact('user'));
+    }
+    //创建注册用户
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// 
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('help','StaticPagesController@help')->name('help');
+Route::get('about','StaticPagesController@about')->name('about');
+//注册
+Route::get('signup','UsersController@signup')->name('signup');
