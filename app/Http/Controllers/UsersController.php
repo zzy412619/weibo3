8cc22c544c67aaa31d90de732cb3a11f3e184d9a
zeyuan <zzy412619@qq.com>
2020-03-03 12:01:36 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,6 +25,15 @@ class UsersController extends Controller
             'password' => 'required|confirmed|min:6'
         ]);
 
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        session()->flash('success','欢迎,您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show',[$user]);
+
 
     }
 }
