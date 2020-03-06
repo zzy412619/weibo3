6cfa09fb2366b28dd7a872682a2353ac3ec1e018
zeyuan <zzy412619@qq.com>
2020-03-06 06:05:35 +0000

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,12 @@ Route::delete('/users{user}','UsersController@destroy')->name('users.destroy');
 
 //激活路由
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+//显示重置密码的邮箱发送页
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+//邮箱发送重置连接
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+//密码更新页面
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+//执行密码更新操作
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
