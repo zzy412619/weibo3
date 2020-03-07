3edbbfe89517fe667b1998e41a00a10a96d44dbd
zeyuan <zzy412619@qq.com>
2020-03-07 12:13:52 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -59,3 +59,8 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 //微博的创建和删除
 Route::resource('statuses','StatusesController',['only' => ['store','destroy']]);
+
+//显示用户的关注人列表
+Route::get('/users/{user}/followings','UsersController@followings')->name('users.followings');
+//显示用户的粉丝列表
+Route::get('/users/{user}/followers','UsersController@followers')->name('users.followers');
