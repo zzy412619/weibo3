c84b7d6c1bc5cbbbb9bf56f58acfa8a4c98baa2d
zeyuan <zzy412619@qq.com>
2020-03-07 08:57:17 +0000

    删除微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -1,4 +1,3 @@
-<!-- 微博动态的发布者，发布内容以及发布日期等信息 -->
 <li class="media mt-4 mb-4">
   <a href="{{ route('users.show', $user->id )}}">
     <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar"/>
@@ -7,4 +6,12 @@
     <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
     {{ $status->content }}
   </div>
+
+  @can('destroy', $status)
+    <form action="{{ route('statuses.destroy', $status->id) }}" method="POST" onsubmit="return confirm('您确定要删除本条微博吗？');">
+      {{ csrf_field() }}
+      {{ method_field('DELETE') }}
+      <button type="submit" class="btn btn-sm btn-danger">删除</button>
+    </form>
+  @endcan
 </li>
