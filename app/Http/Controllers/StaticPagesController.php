916d87d0364ec51836b39656f04e57c557fbd64d
zeyuan <zzy412619@qq.com>
2020-03-07 07:44:20 +0000

    发布微博

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class StaticPagesController extends Controller
 {
     public function home()
--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,6 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 //执行密码更新操作
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+//微博的创建和删除
+Route::resource('statuses','StatusesController',['only' => ['store','destroy']]);
